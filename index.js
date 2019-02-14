ce28fba8aa05b2a04448d1f839fb134e5058021f
Mayra.munguia <mayra.munguia@caffenio.com>
2019-02-14 09:27:06 -0700

    [Feature] se agregó conexión a mlab y configuración de babel

--- index.js
+++ index.js
@@ -4,6 +4,14 @@ import bodyParser from 'body-parser';
 import cors from 'cors';
 
 
+const JsonParser = bodyParser.json();
+const app = express();
+const PORT = process.env.PORT || 6430
+
+mongoose.connect("mongodb://admin:admin123@ds135255.mlab.com:35255/recargasdb");
+
+const db = mongoose.connection;
+
 app.get('/hola', (req,res) => {
     res.send("Hello hola");
 });
